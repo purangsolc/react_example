524298442d3699efbeb490e675b6c19fa33e0696
P Dev <>
2019-07-19 00:02:05 +0530

    add build pack

---
+++ my-app/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HWP = require('html-webpack-plugin');
+module.exports = {
+   entry: path.join(__dirname, '/src/index.js'),
+   output: {
+       filename: 'build.js',
+       path: path.join(__dirname, '/dist')},
+   module:{
+      rules:[{
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+            'file-loader'
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+            'style-loader',
+            'css-loader'
+        ]
+      }]
+   },
+   plugins:[
+       new HWP(
+          {template: path.join(__dirname,'/src/index.html')}
+       )
+   ]
+}
